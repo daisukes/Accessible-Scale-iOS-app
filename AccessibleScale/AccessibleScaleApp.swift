c5434ae3f4d907202f970746c2e52b26ae9900b4
Daisuke Sato <daisukes@cmu.edu>
2021-02-14 12:16:04 -0500

    Initial commit

---
+++ AccessibleScale/AccessibleScaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibleScaleApp.swift
+//  AccessibleScale
+//
+//  Created by Daisuke Sato on 2021/02/14.
+//
+
+import SwiftUI
+
+@main
+struct AccessibleScaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
