c6c33b68abc83a882bd0a1777102fec7e7e443ab
Daisuke Sato <daisukes@cmu.edu>
2021-02-14 21:54:51 -0500

    add view

--- AccessibleScale/ContentView.swift
+++ AccessibleScale/ContentView.swift
@@ -7,15 +7,59 @@
 
 import SwiftUI
 
+extension View {
+    @ViewBuilder func isHidden(_ hidden: Bool, remove: Bool = false) -> some View {
+        if hidden {
+            if !remove {
+                self.hidden()
+            }
+        } else {
+            self
+        }
+    }
+}
+
 struct ContentView: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var measureFat = false
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack (alignment: .trailing) {
+            Toggle(isOn: $measureFat) {
+                Text("Measure Body Composite")
+            }
             .padding()
+            .hidden()
+            
+            HStack {
+                Text(String(format: "%05.2f", modelData.viewData.weight))
+                    .font(.system(size: 60))
+                Text(modelData.viewData.unit.label())
+                    .font(.largeTitle)
+                    .frame(width: 50.0)
+                    .fixedSize(horizontal: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/, vertical: false)
+            }
+            .accessibilityLabel(modelData.viewData.localizedWeightString())
+            
+            HStack {
+                Text(String(format: "%04.1f", modelData.viewData.fat))
+                    .font(.system(size: 60))
+                Text("%")
+                    .font(.largeTitle)
+                    .frame(width: 50.0)
+                    .fixedSize(horizontal: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/, vertical: false)
+            }
+            .accessibilityLabel(modelData.viewData.localizedFatString())
+            .isHidden(!measureFat)
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
---
+++ AccessibleScale/ModelData.swift
@@ -0,0 +1,39 @@
+//
+//  ModelData.swift
+//  AccessibleScale
+//
+//  Created by Daisuke Sato on 2021/02/14.
+//
+
+import Foundation
+
+final class ModelData: ObservableObject {
+    @Published var viewData: ViewData = ViewData()
+}
+
+enum Unit: String {
+    case KiloGram = "Kilo grams"
+    case Pound = "Pounds"
+    
+    func label() -> String {
+        switch(self) {
+        case Unit.KiloGram:
+            return "kg"
+        case Unit.Pound:
+            return "lb"
+        }
+    }
+}
+
+struct ViewData {
+    var weight: Float = 0
+    var fat: Float = 0
+    var unit: Unit = .KiloGram
+    
+    func localizedWeightString() -> String {
+        return String(format: "%.1f %@", weight, unit.rawValue)
+    }
+    func localizedFatString() -> String {
+        return String(format: "%.1f %", fat)
+    }
+}
